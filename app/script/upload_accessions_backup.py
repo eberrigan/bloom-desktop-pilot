cd1b3943587c817b6ed2250c0ac31c98a9af72de
Benfica Fernando <benficaa@Benficas-MacBook-Pro.local>
2025-08-21 13:39:50 -0700

    minor updates to the update_accessions_backup script

--- app/script/upload_accessions_backup.py
+++ app/script/upload_accessions_backup.py
@@ -32,34 +32,40 @@ engine = create_engine(database_string_prod)
 
 for index, row in df.iterrows():
     with engine.begin() as conn:
-        qr_code, accession_id = conn.execute(
+        result = conn.execute(
             text("SELECT qr_code, accession_id FROM cyl_plants WHERE qr_code = :barcode"),
             {"barcode": row["BARCODE"]}
         ).fetchone()
 
-        if accession_id == 7737034:
-            print(f"Update Aceesion :  {qr_code}")
-            '''
-            Check if the accession record alreay exists in the accessions table, 
-            if not make new entry and link with the barcode 
-            else link bacrdoe to exitsing accessions
-            '''
-            existing_accession = conn.execute(
-                text("SELECT * FROM accessions WHERE name = :accession"), {"accession":row["ACCESSION"]}
-            ).fetchone()
-
-            if existing_accession is None:
-                accession_id = conn.execute(
-                    text("INSERT INTO accessions (name) VALUES (:accession) RETURNING id"),{"accession":row["ACCESSION"]}
-                ).scalar()
-
-            else:
-                accession_id, accession_name, created_at = existing_accession
-
-            conn.execute(
-                    text("UPDATE cyl_plants SET accession_id = :accession_id WHERE qr_code = :barcode"),{"accession_id": accession_id, "barcode": row["BARCODE"]}
-            )
-            print(f"Updated accession_id {accession_id} for barcode {row['BARCODE']}")
+        if result is None:
+            print(f"Barcode {row["BARCODE"]} is not found")
         else:
-            print(f"Accession already present : {qr_code}")
+            qr_code, accession_id = result
+            if accession_id == 7737034:
+                print(f"Barcode {qr_code} is mapped to {accession_id}: Unknown")
+                print(f"{qr_code}: Updating Accesion...")
+                '''
+                Check if the accession record alreay exists in the accessions table, 
+                if not make new entry and link with the barcode 
+                else link bacrdoe to exitsing accessions
+                '''
+                existing_accession = conn.execute(
+                    text("SELECT * FROM accessions WHERE name = :accession"), {"accession":row["ACCESSION"]}
+                ).fetchone()
+
+                if existing_accession is None:
+                    accession_id = conn.execute(
+                        text("INSERT INTO accessions (name) VALUES (:accession) RETURNING id"),{"accession":row["ACCESSION"]}
+                    ).scalar()
+
+                else:
+                    accession_id, accession_name, created_at = existing_accession
+
+                conn.execute(
+                        text("UPDATE cyl_plants SET accession_id = :accession_id WHERE qr_code = :barcode"),{"accession_id": accession_id, "barcode": row["BARCODE"]}
+                )
+                print(f"Updated accession_id {accession_id} for barcode {row['BARCODE']}")
+            else:
+                print(f"Barcode {qr_code} is mapped to {accession_id}")
+                print(f"Valid Accession already present : {qr_code} Skip update")
 
